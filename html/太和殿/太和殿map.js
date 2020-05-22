26008b7ccadb8c79bfd94576f9253bf6fc0b8ec8
lizhaoyun <2571770878@qq.com>
2020-05-22 17:31:35 +0800

    taihe

--- html/太和殿/太和殿map.js
+++ html/太和殿/太和殿map.js
@@ -1,16 +1,16 @@
 $(function() {
     var e = new BMap.Map("main")
-      , t = new BMap.Point(116.403694,39.92013);
+      , t = new BMap.Point(116.403397,39.923671);
     var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
 	var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
 	var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL});
     e.centerAndZoom(t, 17),
-    e.addControl(new BMap.MapTypeControl({
-        mapTypes: [BMAP_NORMAL_MAP, BMAP_HYBRID_MAP]
-    })),
+    // e.addControl(new BMap.MapTypeControl({
+    //     mapTypes: [BMAP_NORMAL_MAP, BMAP_HYBRID_MAP]
+    // })),
     e.addControl(top_left_control),
     e.addControl(top_left_navigation),
-    e.addControl(top_right_navigation),
+    // e.addControl(top_right_navigation),
 
     e.enableScrollWheelZoom(!0),
     e.setMapType(BMAP_NORMAL_MAP);
@@ -19,15 +19,14 @@ $(function() {
     });
     a.setAnimation(BMAP_ANIMATION_BOUNCE),
     e.addOverlay(a);
-    var n = new BMap.InfoWindow('<div style="width: 250px"><p>故宫</p><p>开放时间：9：00-17:30</p></div>',{
-        width: 230,
-        height: 100,
-        // title: "<strong>SPA 富应用开发</strong>"
-    });
-    a.openInfoWindow(n),
-    a.addEventListener("click", function() {
-        e.openInfoWindow(n, t)
-    });
+    // var n = new BMap.InfoWindow('<div style="width: 250px"><p>故宫</p><p>开放时间：9：00-17:30</p></div>',{
+    //     width: 230,
+    //     height: 100,
+    // });
+    // a.openInfoWindow(n),
+    // a.addEventListener("click", function() {
+    //     e.openInfoWindow(n, t)
+    // });
 
 	
 	
