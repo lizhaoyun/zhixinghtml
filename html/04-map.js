71b9aaa9e8ac6b2ade0b933d64e6fc964ea05c97
lizhaoyun <2571770878@qq.com>
2020-05-22 17:15:23 +0800

    change

--- html/04-map.js
+++ html/04-map.js
@@ -1,10 +1,17 @@
 $(function() {
     var e = new BMap.Map("main")
       , t = new BMap.Point(116.403694,39.92013);
-    e.centerAndZoom(t, 25),
+    var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+	var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+	var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL});
+    e.centerAndZoom(t, 17),
     e.addControl(new BMap.MapTypeControl({
         mapTypes: [BMAP_NORMAL_MAP, BMAP_HYBRID_MAP]
     })),
+    e.addControl(top_left_control),
+    e.addControl(top_left_navigation),
+    e.addControl(top_right_navigation),
+
     e.enableScrollWheelZoom(!0),
     e.setMapType(BMAP_NORMAL_MAP);
     var a = new BMap.Marker(t,{
@@ -20,5 +27,8 @@ $(function() {
     a.openInfoWindow(n),
     a.addEventListener("click", function() {
         e.openInfoWindow(n, t)
-    })
+    });
+
+	
+	
 });
