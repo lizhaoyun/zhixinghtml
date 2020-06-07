66404af0e434cb1315830c823ae230ec4e1dc3a3
lizhaoyun <2571770878@qq.com>
2020-06-08 07:56:23 +0800

    update

---
+++ html/map/陆羽茶社map.js
@@ -0,0 +1,17 @@
+$(function() {
+    var e = new BMap.Map("main")
+      , t = new BMap.Point(108.977461,34.220437);
+    var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+	var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+	var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL});
+    e.centerAndZoom(t, 17),
+    e.addControl(top_left_control),
+    e.addControl(top_left_navigation),
+    e.enableScrollWheelZoom(!0),
+    e.setMapType(BMAP_NORMAL_MAP);
+    var a = new BMap.Marker(t,{
+        title: "大唐芙蓉园"
+    });
+    a.setAnimation(BMAP_ANIMATION_BOUNCE),
+    e.addOverlay(a);
+});
